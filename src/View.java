9c0173b5ea6ec1b8499d88d8f71e6cc5040f59b6
Jon Wettersten <jwettersten@ideo.com>
2014-09-12 17:09:43 -0500

    added create() and changed output to support ints vs. chars

--- src/View.java
+++ src/View.java
@@ -1,31 +1,29 @@
-/**
- * 
- */
-
+import java.util.Observable;
+import java.util.Observer;
 
 /**
  * @author jwettersten
  *
  */
-public class View {
+public class View implements Observer {
 	
-	Board gameBoard;
-	Controller gameController;
+	private Board gameBoard;
+	private Controller gameController;
 	
 	public View(Controller controller, Board board) {
 		this.gameController = controller;
 		this.gameBoard = board;
 		
-		// register this view as an observer as a model if necessary
-		//this.gameBoard.re
-		
-		displayMessage("Welcome to the game of Tic Tac Toe!");
-		
-		displayBoard();
+		board.addObserver(this);
+
 	}
 	
-	/** Display the game board */
-	public void displayBoard() {
+	public void create() {
+		displayMessage("Welcome to the game of Tic Tac Toe!");
+		displayGameBoard();
+	}
+	
+	public void displayGameBoard() {
       
 	   for (int row = 0; row < gameBoard.getBoard().length; ++row) {
          
@@ -36,14 +34,14 @@ public class View {
 			   if (col != gameBoard.getBoard().length - 1) {
 				   System.out.print("|");   // print vertical partition
 			   }
-         }
+		   }
 		   
          System.out.println();
          
          if (row != gameBoard.getBoard().length - 1) {
             System.out.println("-----------"); // print horizontal partition
          }
-      }
+	   }
 	   
       System.out.println();
    }
@@ -53,12 +51,16 @@ public class View {
 	}
    
    	/** Output the value of the index */
-	private void outputValue(char content) {
-      switch (content) {
-         case '\u0000':  System.out.print("   "); break;
-         case 'O': System.out.print(" O "); break;
-         case 'X':  System.out.print(" X "); break;
-      }
-   }
+	private void outputValue(int content) {
+		switch (content) {
+			case 0:  System.out.print("   "); break;
+			case 2: System.out.print(" O "); break;
+			case 1:  System.out.print(" X "); break;
+		}
+	}
+	
+	public void update(Observable obs, Object x) {
+		displayGameBoard();
+	}
 
 }
