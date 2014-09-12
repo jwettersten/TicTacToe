3e4ef3ef4dc249f898257a41704c9740c9303d9c
Jon Wettersten <jwettersten@ideo.com>
2014-09-12 17:07:08 -0500

    refactored game setup from main to Controller

--- src/Controller.java
+++ src/Controller.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+import java.util.Scanner;
 
 /**
  * @author jwettersten
@@ -8,31 +6,50 @@
  */
 public class Controller {
 	
-	Board board;
-	View view;
+	private Board board;
+	private Player human;
+	private AIPlayer computer;
+	private View view;
+	private Scanner consoleUserInput = new Scanner(System.in);
 	
 	public Controller(Board board) {
 		this.board = board;
 		view = new View(this, board);
+		
+		view.create();
+		setupPlayers();
+		nextMove();
 	}
 	
-	public void startGame() {
-		// setup the game
-		// setup score?
-	}
 	
-	public void endGame() {
-		// end the game
-		// trigger final score
+	
+	public void playerMove(Player player) {
+		
+		boolean isMoveSuccessful = false;  
+		
+		do {
+			
+			System.out.print("Enter the row and column of your next move, e.g. 1 2 (first row [space] second column): ");
+		      
+			int row = consoleUserInput.nextInt() - 1; 
+			int column = consoleUserInput.nextInt() - 1;
+			
+			if (attemptPlayerMove(row, column, player)) {
+				isMoveSuccessful = true;  
+			} else {
+				System.out.println("The position (" + (row + 1) + "," + (column + 1) + ") is not available. Try again!");
+			}
+			
+		} while (!isMoveSuccessful);
 	}
 
 	public boolean attemptPlayerMove(int rowPosition, int colPosition, Player player) {
 		
 		boolean returnValue = false;
 		
-		if (board.boardPositionAvailable(rowPosition, colPosition)) {
+		if (board.isBoardPositionAvailable(rowPosition, colPosition)) {
 			// set the position at the positionIndex for the player type
-			this.board.setPlayerBoardPosition(rowPosition, colPosition, player.getType());
+			this.board.setPlayerBoardPosition(rowPosition, colPosition, player.getMark());
 			
 			returnValue = true;
 			
@@ -43,11 +60,29 @@ public class Controller {
 			returnValue = false;
 		}
 		
-		// Update display - consider refactoring view as an observer of the TTTBoard model
-		this.view.displayBoard();
-		
 		return returnValue;
 		
+	}
+	
+	public void nextMove() {
+		/**
+		 *  Accept and process player moves (row and column) until there is a winner
+		 */
+		do {
+			playerMove(human);
+			
+			// TODO computer move calls AI method...
+			
+		} while (true); // repeat until the game is over
+	}
+	
+	public void setupPlayers() {
+		// Create player instances
+		System.out.println("What is your name Player X?");
+		String newPlayerName = consoleUserInput.next(); 
+		human = new Player(newPlayerName, 1);
+		computer = new AIPlayer(board);
+		System.out.println("Welcome to Tic Tac Toe " + human.getName() + "!");
 	}
 	
 }
