389c568655eb0ca97c680f5f55077bd904a28cc2
Jon Wettersten <jwettersten@ideo.com>
2014-09-13 08:19:52 -0500

    added board dimensions as static variables, added board scoring logic

--- src/Board.java
+++ src/Board.java
@@ -7,25 +7,18 @@ import java.util.Observable;
 public class Board extends Observable {
 
 	private int[][] board;
+	private static int ROWS = 3;
+	private static int COLUMNS = 3;
 	
 	public Board() {
-		this.board = new int[3][3];
+		this.board = new int[ROWS][COLUMNS];
 	}
 	
 	public int[][] getBoard() {
 		return board;
 	}
 	
-	public void setPlayerBoardPosition(int row, int column, int playerType) {
-		
-		this.board[row][column] = playerType;
-		
-		setChanged();
-		notifyObservers();
-	}
-	
 	public boolean isBoardPositionAvailable(int row, int column) {
-		System.out.println("index value: " + this.board[row][column]);
 		
 		if (this.board[row][column] > 0) {
 			return false;
@@ -35,29 +28,43 @@ public class Board extends Observable {
 		
 	}
 	
+	public void setPlayerBoardPosition(int row, int column, int playerMark) {
+		
+		this.board[row][column] = playerMark;
+		
+		setChanged();
+		notifyObservers();
+	}
+	
+	
 	/**
-	 * This 
+	 * The following 2 winner scoring pattern + hasPlayerWon functions came from the AI example from:
+	 * https://www3.ntu.edu.sg/home/ehchua/programming/java/JavaGame_TicTacToe_AI.html
 	 */
+	/** Returns true if thePlayer wins */
+	public boolean hasPlayerWon(int playerMark) {
+		int pattern = 0b000000000;  // 9-bit pattern for the 9 cells
+		for (int row = 0; row < ROWS; ++row) {
+			for (int col = 0; col < COLUMNS; ++col) {
+				if (board[row][col] == playerMark) {
+					pattern |= (1 << (row * ROWS + col));
+				}
+			}
+		}
+		   
+		for (int winningPattern : winningPatterns) {
+			if ((pattern & winningPattern) == winningPattern) return true;
+		}
+		   
+		return false;
+	}
+	
 	private int[] winningPatterns = {
 			0b111000000, 0b000111000, 0b000000111, // rows
 			0b100100100, 0b010010010, 0b001001001, // cols
 			0b100010001, 0b001010100               // diagonals
 	};
  
-   /** Returns true if thePlayer wins */
-   public boolean hasWon(int player) {
-	   int pattern = 0b000000000;  // 9-bit pattern for the 9 cells
-	   for (int row = 0; row < 3; ++row) {
-         for (int col = 0; col < 3; ++col) {
-            if (board[row][col] == player) {
-               pattern |= (1 << (row * 3 + col));
-            }
-         }
-      }
-      for (int winningPattern : winningPatterns) {
-         if ((pattern & winningPattern) == winningPattern) return true;
-      }
-      return false;
-   }
+   
 	
 }
