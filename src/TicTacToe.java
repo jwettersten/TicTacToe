e391fde6506a77beeac9536af85cc2470cebf2a7
Jon Wettersten <jwettersten@ideo.com>
2014-09-16 09:38:10 -0500

    cleaned out unnecessary comments

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -7,7 +7,6 @@ public class TicTacToe {
 	
 	public static void main(String[] args) {
 		
-		// Setup the game
 		Board gameBoard = new Board();
 		Controller gameController = new Controller(gameBoard);
 		
