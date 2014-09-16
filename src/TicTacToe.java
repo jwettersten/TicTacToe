8cbb33a748693a2ca2ea43e45fa39cbc28f6db0d
Jon Wettersten <jwettersten@ideo.com>
2014-09-16 09:37:14 -0500

    extracted methods from Controller into Main

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -7,8 +7,14 @@ public class TicTacToe {
 	
 	public static void main(String[] args) {
 		
+		// Setup the game
 		Board gameBoard = new Board();
 		Controller gameController = new Controller(gameBoard);
+		
+		gameController.createHumanPlayerWithName();
+		gameController.createComputerPlayer();
+		
+		gameController.playGame();
 
 	}
 
